3c8bd7bc8cee45d581eb44e9c7f5609a3ffea545
jmmarao <jmmarao@gmail.com>
2022-08-22 22:06:15 -0300

    Class 04: tags, enable JRE and OS

---
+++ src/test/java/br/edu/ifsp/class04/CustomerTest.java
@@ -0,0 +1,47 @@
+package br.edu.ifsp.class04;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+    private static Customer customer;
+
+    @BeforeAll
+    public static void initializeCustomer() {
+        customer = new Customer("", "");
+    }
+
+    @Test
+    @EnabledOnOs({OS.LINUX})
+    @Tag("ICorrect")
+    public void testCase1() {
+        customer.setName("Moon Guard");
+        assertEquals("Moon Guard", customer.getName());
+    }
+
+    @Test
+    @EnabledOnOs({OS.WINDOWS, OS.LINUX})
+    @Tag("ToCorrect")
+    public void testCase2() {
+        customer.setCpf("000.000.000-00");
+        assertEquals("000.000.000-00", customer.getCpf());
+    }
+
+    @Test
+    @EnabledOnJre({JRE.JAVA_17})
+    @Tag("ToCorrect")
+    public void testCase3() {
+        assertTrue(true);
+    }
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_17, max = JRE.JAVA_18)
+    @Tag("ICorrect")
+    public void testCase4() {
+        assertTrue(true);
+    }
+}

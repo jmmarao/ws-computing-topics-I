606605a4a58a4d3b6c0eb8d46aa0b83bb5970f59
jmmarao <jmmarao@gmail.com>
2022-11-28 14:48:23 -0300

    Class 06 adjusts

--- src/main/java/br/edu/ifsp/class06/SeleniumTest.java
+++ src/main/java/br/edu/ifsp/class06/SeleniumTest.java
@@ -5,30 +5,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumTest {
+public class SeleniumTestAntigo {
     public static void main(String[] args) throws InterruptedException {
-        String userPath = System.getProperty("user.dir");
-        System.setProperty("webdriver.chrome.driver", userPath + "/lib/chromedriver/chromedriver.exe");
-
+//        String projectPath = System.getProperty("user.dir");
+//        String chromeDriverPath = projectPath + "\\lib\\chromedriver\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", "src/main/java/br/edu/ifsp/class06/driver/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://scl.ifsp.edu.br");
-
-        WebElement plateObj = driver.findElement(By.id("iptPlaca"));
-        plateObj.sendKeys("ABC-9U34");
-
-        WebElement brandObj = driver.findElement(By.id("iMarca"));
-        brandObj.sendKeys("Fiat");
-
-        WebElement modelObj = driver.findElement(By.xpath("/html/body/form/input[3]"));
-        modelObj.sendKeys("Uno");
-
-        WebElement showObj = driver.findElement(By.xpath("/html/body/button[1]"));
-        showObj.click();
-
-        WebElement linkObj = driver.findElement(By.partialLinkText("IFSP"));
-        linkObj.click();
-
-        Thread.sleep(10000);
-        driver.quit();
+        driver.get("file:/home/jones/Documents/repository/ifsp/ws-computing-topics-I/src/main/java/br/edu/ifsp/class06/index.html");
+        WebElement btnEnviar = driver.findElement(By.xpath("/html/body/form/input[2]"));
+        btnEnviar.click();
+        WebElement iptLogin = driver.findElement(By.id("idLogin"));
+        WebElement par1 = driver.findElement(By.id("p1"));
+        String textoParagrafo = par1.getText();
+        String esperada = "Este campo é requerido";
+        String login = iptLogin.getAttribute("value");
+        String corAtual = par1.getCssValue("color");
+        String corEsperada = "rgba(255, 0, 0, 1)";
+        if(login.isEmpty()){
+            if(esperada.compareTo(textoParagrafo)==0){
+                System.out.println("A saída está conforme o esperado");
+                if(corEsperada.compareTo(corAtual)==0)
+                    System.out.println("A cor do parágrafo está correta");
+                else
+                    System.out.println("A cor do parágrafo não está correta");
+            }else{
+                System.out.println("Desenvolvedor, você cometeu um erro catastrófico...");
+            }
+        }
     }
 }
